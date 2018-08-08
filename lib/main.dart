c42df22b5e7abd525b627f9978f18f9dc688af65
MrAnand05 <anandguptastar@gmail.com>
2018-08-09 00:39:39 +1000

    final Rocket

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'dart:math' as math;
 void main() => runApp(new MyApp());
 
 class RotationYTransition extends AnimatedWidget {
-  
   const RotationYTransition({
     Key key,
     @required Animation<double> turns,
@@ -39,47 +38,59 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Home extends StatefulWidget{
-  HomeState createState()=>  HomeState();
+class Home extends StatefulWidget {
+  HomeState createState() => HomeState();
 }
 
-class HomeState extends State<Home> with SingleTickerProviderStateMixin{
+class HomeState extends State<Home> with SingleTickerProviderStateMixin {
   AnimationController _controller;
   Animation<double> animation;
   Animation<Offset> moveAnimation;
   Animation<double> scaleAnimation;
   @override
-    void initState() {
-      _controller=AnimationController(duration: Duration(seconds: 2),vsync: this);
-      //animation=Tween(begin: 0.0,end: 5.0).animate(_controller);
-      //scaleAnimation=Tween(begin: 0.0,end: -1.0).animate(_controller);
-      //moveAnimation=Tween<Offset>(begin: const Offset(0.0, -1.0),end: Offset.zero).animate(_controller);
-      super.initState();
-    }
-  void callController(){
-    animation=Tween(begin: 0.0,end: 20.0).animate(_controller);
-    scaleAnimation=Tween(begin: 0.0,end: 1.0).animate(_controller);
-    moveAnimation=Tween<Offset>(begin: const Offset(0.0, 1.0),end: Offset(0.0,0.0)).animate(_controller);
-    if(_controller.status==AnimationStatus.completed){
+  void initState() {
+    _controller =
+        AnimationController(duration: Duration(seconds: 2), vsync: this);
+    animation = Tween(begin: 0.0, end: 5.0).animate(_controller);
+    scaleAnimation = Tween(begin: 0.0, end: -1.0).animate(_controller);
+    moveAnimation =
+        Tween<Offset>(begin: const Offset(0.0, -1.0), end: Offset.zero)
+            .animate(_controller);
+    super.initState();
+  }
+
+  void callController() {
+    animation = Tween(begin: 0.0, end: 15.0).animate(_controller);
+    scaleAnimation = Tween(begin: 1.0, end: 0.3).animate(_controller);
+    moveAnimation =
+        Tween<Offset>(begin: const Offset(0.0, 0.0), end: Offset(0.0, -4.5))
+            .animate(_controller);
+    if (_controller.status == AnimationStatus.completed) {
+      print('reverse');
       _controller.reverse();
-    }else{
+    } else {
+      print('forward');
       _controller.forward();
     }
   }
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         children: <Widget>[
-          ScaleTransition(
-            scale: scaleAnimation,
-          child:SlideTransition(position: moveAnimation,
-          child:RotationYTransition(
-            turns: animation,
-          child:Image.asset('assets/FlutterLogo2.jpg',),
-          ),
-          ),
+          SlideTransition(
+            position: moveAnimation,
+             child:ScaleTransition(
+              scale: scaleAnimation,
+              child: RotationYTransition(
+                turns: animation,
+                child: Image.asset(
+                  'assets/FlutterLogo2.jpg',
+                ),
+              ),
+            ),
           ),
         ],
       ),
